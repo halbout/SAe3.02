478e1a40c13b02fd62dadae610cdb1c3a4b01a66
halbout <ernest.halbout@uha.fr>
2022-11-29 15:21:42 +0100

    git

--- socket-master/client.py
+++ socket-master/client.py
@@ -2,19 +2,19 @@ import socket
 
 try:
 
-    def sendCmd(client_socket):
-
-        BYE = "bye"
-        ARRET = "arret"
+    BYE = "bye"
+    ARRET = "arret"
+    data = ""
+    while data != BYE and data != ARRET:
         message = ""
-        client = socket.socket()
-        client.connect(('127.0.0.1', 10000))
+        client_socket = socket.socket()
+        client_socket.connect(('127.0.0.1', 10000))
 
         while message != BYE and message != ARRET and data != BYE and data != ARRET:
             message = input("")
-            client.send(message.encode())
+            client_socket.send(message.encode())
 
-        client.close()
+        client_socket.close()
 
 except KeyboardInterrupt:
     print("La connexion au serveur a été interrompue")
--- socket-master/serveur.py
+++ socket-master/serveur.py
@@ -9,28 +9,27 @@ try:
     while res != KILL and cmd != KILL:
         server = socket.socket()
         server.bind(('127.0.0.1', 10000))
-        server.listen(1)
-        conn, address = server.accept()
-        res = cmd = ""
+        while res != KILL and cmd != KILL and res != RESET and cmd != RESET:
+            server.listen(1)
+            conn, address = server.accept()
+            res = cmd = ""
 
-        while res != KILL and cmd != KILL and res != RESET and cmd != RESET and res != DISCONNECT and cmd != DISCONNECT:
-            cmd = conn.recv(1024).decode()
-            print(cmd)
-            if cmd == DISCONNECT:
-                res = DISCONNECT
-                conn.send(res.encode())
-            elif cmd == RESET:
-                res = RESET
-                conn.send(res.encode())
-            elif cmd == KILL:
-                res = KILL
-                conn.send(res.encode())
-            else:
-                start = res = input("")
+            while res != KILL and cmd != KILL and res != RESET and cmd != RESET and res != DISCONNECT and cmd != DISCONNECT:
+                cmd = conn.recv(1024).decode()
+                print(cmd)
+                if cmd == DISCONNECT:
+                    res = DISCONNECT
+                    conn.send(res.encode())
+                elif cmd == RESET:
+                    res = RESET
+                    conn.send(res.encode())
+                elif cmd == KILL:
+                    res = KILL
+                    conn.send(res.encode())
+                else:
+                    res = input("")
 
-                end = conn.send(res.encode())
-
-        conn.close()
+            conn.close()
         server.close()
 
 except ConnectionAbortedError:
