1bebd94a33b0a9c6df7366ace68a294bf4bf84db
halbout <ernest.halbout@uha.fr>
2022-11-30 16:07:55 +0100

    GUI

--- Interface graphique/GUI.py
+++ Interface graphique/GUI.py
@@ -13,53 +13,49 @@ class Client(QMainWindow):
         grid = QGridLayout()
         widget.setLayout(grid)
 
-        quit = QPushButton("Exit")
+        exit = QPushButton("Exit")
         lab1 = QLabel("Enter your command")
         self.__cmd = QLineEdit("")
-        send = QPushButton("Send")
+        self.__Send = QPushButton("Send")
         help = QPushButton("Help")
         OS = QComboBox()
         OS.addItem("DOS")
         OS.addItem("Linux")
         OS.addItem("Powershell")
         lab2 = QLabel("Système \nd'exploitation")
-        self.__res = QLabel("")
 
-        grid.addWidget(quit, 4, 2)
+        grid.addWidget(exit, 4, 2)
         grid.addWidget(lab1, 0, 1)
         grid.addWidget(self.__cmd, 1, 1)
-        grid.addWidget(send, 2, 1)
+        grid.addWidget(self.__Send, 2, 1)
         grid.addWidget(help, 4, 0)
         grid.addWidget(OS, 1, 2)
         grid.addWidget(lab2, 0, 2)
-        grid.addWidget(self.__res, 3, 1)
 
-        quit.clicked.connect(self.__actionQuit)
-        send.clicked.connect(self.__actionSend)
+        exit.clicked.connect(self.__actionQuit)
+        self.__Send.clicked.connect(self.__actionSend)
         help.clicked.connect(self.__actionHelp)
         self.setWindowTitle("Gestion des serveurs")
-
 
     def __actionQuit(self):
         QCoreApplication.exit(0)
 
-    def __actionRes(self):
-        client = socket.socket()
-        client.connect(('127.0.0.1', 30000))
-        self.__res = client.recv(1024).decode()
-        client.close()
-
     def __actionSend(self):
+        host = "localhost"
+        port = 30000
         client = socket.socket()
-        client.connect(('127.0.0.1', 30000))
-        client.send(self.__cmd.encode())
+        client.connect((host, port))
+        client.send(self.__Send.encode())
+        res = QMessageBox()
+        res.setWindowTitle("Résultats")
+        res.setText(client.recv(1024).decode())
         client.close()
+        resultat = res.exec()
 
     def __actionHelp(self):
         self.__message = QMessageBox()
-        self.__message.setText("Dans un premier temps choisissez votre système d'exploitation via le menu déroulant.\nEnsuite tapez une commande dans la zone de texte approprié parmi les suivantes : RAM, CPU, IP, Name."
-                               "\nVous pouvez également tapez des commandes de création et de suppression sur les serveurs")
+        self.__message.setText("Dans un premier temps choisissez votre système d'exploitation via le menu déroulant.\n \nEnsuite tapez une commande dans la zone de texte approprié parmi les suivantes : RAM, CPU, IP, Name."
+                               "\n \nVous pouvez également tapez des commandes de création et de suppression sur les serveurs")
         self.__message.setInformativeText("Ces indications peuvent vous aidez à mieux comprendre le fonctionnement de cette interface graphique.")
         self.__message.setWindowTitle("Guide d'utilisation")
         msg = self.__message.exec()
-
