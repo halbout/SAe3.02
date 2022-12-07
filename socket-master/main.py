f178e5cfc51d4a14a674125e66f5c9744343c69a
halbout <ernest.halbout@uha.fr>
2022-12-07 14:18:38 +0100

    connexion socket

--- socket-master/main.py
+++ socket-master/main.py
@@ -1,6 +1,18 @@
 import sys
-import serveur
 import client
 
 if __name__ == "__main__":
-    sys.exit(main())
+
+    print(sys.argv)
+    if len(sys.argv) < 3:
+        client = Client("0.0.0.0", 10000)
+    else:
+        host = sys.argv[1]
+        port = int(sys.argv[2])
+        client = Client(host, port)
+
+    client.start()
+    client.join()
+
+    client.connect()
+    client.echange()
