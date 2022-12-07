fefa2c8cc517e342158d3c22177536ae1bdef245
halbout <ernest.halbout@uha.fr>
2022-12-07 13:55:20 +0100

    client assynchrone

--- socket-master/client asynchrone.py
+++ socket-master/client asynchrone.py
@@ -5,40 +5,40 @@ class Client():
     def __init__(self, host, port):
         self.__host = host
         self.__port = port
-        self.__sock = socket.socket()
+        self.__client = socket.socket()
         self.__thread = None
 
     def connect(self) -> int:
         try:
-            self.__sock.connect((self.__host, self.__port))
+            self.__client.connect((self.__host, self.__port))
         except ConnectionRefusedError:
-            print("serveur non lancé ou mauvaise information")
+            print("serveur non disponible ou mauvaise saisie")
             return -1
         except ConnectionError:
             print("erreur de connection")
             return -1
         else:
-            print("connexion réalisée")
+            print("connexion réussie")
             return 0
 
-    def dialogue(self):
+    def echange(self):
         msg = ""
-        self.__thread = threading.Thread(target=self.__reception, args=[self.__sock, ])
+        self.__thread = threading.Thread(target=self.__recue, args=[self.__client, ])
         self.__thread.start()
         while msg != "kill" and msg != "disconnect" and msg != "reset":
             msg = self.__envoi()
         self.__thread.join()
-        self.__sock.close()
+        self.__client.close()
 
     def __envoi(self):
-        msg = input("client: ")
+        msg = input("Commande :  ")
         try:
-            self.__sock.send(msg.encode())
+            self.__client.send(msg.encode())
         except BrokenPipeError:
-            print("erreur, socket fermée")
+            print("connection fermé")
         return msg
 
-    def __reception(self, conn):
+    def __recue(self, conn):
         msg = ""
         while msg != "kill" and msg != "disconnect" and msg != "reset":
             msg = conn.recv(1024).decode()
